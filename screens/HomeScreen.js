b451a08e9bbd06e416693f330ed5f678484d60d7
Yago Ernandes <yago700@gmail.com>
2019-10-26 21:23:05 -0300

    beta 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,74 +1,165 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import React, { useState, useEffect } from 'react'
+import {
+  AsyncStorage,
+  Platform,
+  Text,
+  StyleSheet,
+  View,
+  Dimensions
+} from 'react-native'
+import MapView from 'react-native-maps'
+import Constants from 'expo-constants'
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
 import axios from 'axios'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function HomeScreen() {
+  ;[state, setState] = useState({
+    location: null,
+    errorMessage: null
+  })
+  ;[linhas, setLinhas] = useState({})
+  ;[buses, setBuses] = useState({})
+  ;[allBuses, setAllBuses] = useState([])
 
-	[state, setState] = useState({
-		location: null,
-		errorMessage: null,
-	})
+  useEffect(() => {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      setState(s => ({
+        ...s,
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+      }))
+    } else {
+      _getLocationAsync()
+    }
+  }, [])
 
-	useEffect(() => {
-		if (Platform.OS === 'android' && !Constants.isDevice) {
-			setState(s => ({
-				...s,
-				errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-			}));
-		} else {
-			_getLocationAsync();
-		}
-	}, [])
+  useEffect(() => {
+    _retrieveData()
+    const id = setInterval(_retrieveData, 10000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
 
-	const getBusLocation = async ()=>{
-		const response = await axios.get('https://www.sistemas.dftrans.df.gov.br/gps/linha/932.1/geo/recent')
-		setState(s=>({...s, bus: response.data.features}))
-		// state.bus.map(bus=>console.log(`Latitude: ${bus.geometry[1]} | Longitude: ${bus.geometry[0]}`))
-		state.bus.map(bus=>console.log(`Location: ${JSON.stringify(bus.geometry)}`))
-	}
+  const _retrieveData = async () => {
+    console.log('----------------------------------------------------')
+    try {
+      const linhasStored = await AsyncStorage.getAllKeys()
+      if (linhasStored !== null) {
+        linhasToAdd = {}
+        for (linha in linhasStored) {
+          const temp = await AsyncStorage.getItem(linhasStored[linha])
+          linhasToAdd[linhasStored[linha]] = JSON.parse(temp)
+        }
+        setLinhas(linhasToAdd)
+      }
+    } catch (error) {
+      console.log('[ ERRO - Retrieving ]: ', error)
+    }
+    getAllBusLocations()
+  }
 
-	useEffect(()=>{
-		setInterval(() => { getBusLocation() }, 1000)
-	},[])
+  const getBusesLocation = async linhaCode => {
+    console.log(`Consultando a url para ${linhaCode}`)
+    const response = await axios.get(url(linhaCode))
+    setBuses(old => ({ ...old, [linhaCode]: response.data.features }))
+    makeBusesList()
+  }
 
-	_getLocationAsync = async () => {
-		let { status } = await Permissions.askAsync(Permissions.LOCATION);
-		if (status !== 'granted') {
-			setState(s => ({
-				...s,
-				errorMessage: 'Permission to access location was denied',
-			}));
-		}
+  const url = linha =>
+    `https://www.sistemas.dftrans.df.gov.br/gps/linha/${linha}/geo/recent`
 
-		let location = await Location.getCurrentPositionAsync({});
-		setState(s => ({ ...s, location }));
-	};
+  const getAllBusLocations = () => {
+    const keysIndexes = Object.keys(linhas)
+    for (linha in Object.keys(linhas)) {
+      console.log('get: ', keysIndexes[linha])
+      console.log('Linha to get: ', linhas[keysIndexes[linha]].code)
+      getBusesLocation(linhas[keysIndexes[linha]].code)
+    }
+    makeBusesList()
+  }
 
+  const makeBusesList = () => {
+    const tempList = []
+    Object.keys(buses).forEach(linhaCode => {
+      buses[linhaCode].forEach((bus, index) => {
+        tempColor =
+          (linhas && linhas[linhaCode] && linhas[linhaCode].color) || '#555555'
+        bus.color = tempColor
+        tempList.push(bus)
+        console.log(
+          `${index + 1} - Onibus em circulação: ${
+            bus.properties.numero
+          } - Cor: ${bus.color}`
+        )
+      })
+    })
+    setAllBuses(tempList)
+  }
 
-	return (
-		<View style={styles.container}>
-			<MapView style={styles.mapStyle}>
-				{
-					state.bus && state.bus.map(bus=><MapView.Marker pinColor={'#F00'} coordinate={{latitude: bus.geometry.coordinates[1], longitude: bus.geometry.coordinates[0]}} />)
-				}
-			</MapView>
-		</View>
-	);
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION)
+    if (status !== 'granted') {
+      setState(s => ({
+        ...s,
+        errorMessage: 'Permission to access location was denied'
+      }))
+    }
+
+    let location = await Location.getCurrentPositionAsync({})
+    setState(s => ({ ...s, location }))
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: -15.7509644,
+          longitude: -47.9596552,
+          latitudeDelta: 0.4,
+          longitudeDelta: 0.4
+        }}>
+        {allBuses.map(bus => {
+          return (
+            <MapView.Marker
+              key={bus.properties.numero}
+              anchor={{ x: 0.5, y: 0.75 }}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+              coordinate={{
+                latitude: bus.geometry.coordinates[1],
+                longitude: bus.geometry.coordinates[0]
+              }}>
+              <Text>{bus.properties.linha}</Text>
+              <Ionicons
+                name='md-bus'
+                size={22}
+                style={{ marginRight: 10 }}
+                color={bus.color || '#555'}
+              />
+            </MapView.Marker>
+          )
+        })}
+      </MapView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	mapStyle: {
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
-	},
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
+})
--- screens/OptionsScreen.js
+++ screens/OptionsScreen.js
@@ -1,27 +1,171 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React, { useState, useEffect } from 'react'
+import {
+  FlatList,
+  StyleSheet,
+  Alert,
+  View,
+  TextInput,
+  Text,
+  Button,
+  Modal,
+  ListView,
+  TouchableHighlight
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { AsyncStorage } from 'react-native'
+import { TriangleColorPicker } from 'react-native-color-picker'
 
-export default function LinksScreen() {
+export default function OptionsScreen() {
+  ;[text, setText] = useState('')
+  ;[modalVisible, setModalVisible] = useState(false)
+  ;[modalSelected, setModalSelected] = useState({})
+  ;[linhas, setLinhas] = useState([])
+
+  useEffect(() => {
+    _retrieveData()
+  }, [])
+
+  const _storeData = async data => {
+    try {
+      await AsyncStorage.setItem(data.code, JSON.stringify(data))
+      _retrieveData()
+    } catch (error) {
+      console.log('[ ERRO - Storing ]: ', error)
+    }
+  }
+
+  const _retrieveData = async () => {
+    try {
+      const linhasStored = await AsyncStorage.getAllKeys()
+      if (linhasStored !== null) {
+        linhasToAdd = []
+        for (linha in linhasStored) {
+          const temp = await AsyncStorage.getItem(linhasStored[linha])
+          linhasToAdd.push(JSON.parse(temp))
+        }
+        setLinhas(linhasToAdd)
+      }
+    } catch (error) {
+      console.log('[ ERRO - Retrieving ]: ', error)
+    }
+  }
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible)
+  }
+
+  const _formatText = text => {
+    return text.replace(',', '.')
+  }
+
+  const addLinha = () => {
+    console.log(linhas)
+    _storeData({
+      code: _formatText(text),
+      color: '',
+      addeAt: Date.now()
+    })
+    setText('')
+  }
+
+  const selectItem = item => () => {
+    setModalSelected(item)
+    toggleModal()
+  }
+
+  const changeColor = (item, color) => {
+    const temp = item
+    temp.color = color
+    _storeData(temp)
+  }
+
+  const removeItem = item => () => {
+    AsyncStorage.removeItem(item.code)
+    _retrieveData()
+  }
+
   return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+    <View style={{ paddingHorizontal: 10 }}>
+      <View style={{ flexDirection: 'row', marginVertical: 15 }}>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            flex: 1,
+            marginRight: 15,
+            borderRadius: 5,
+            paddingHorizontal: 15
+          }}
+          onChangeText={t => setText(t)}
+          value={text}
+        />
+        <Button title='Adicionar' onPress={addLinha} />
+      </View>
+      <FlatList
+        data={linhas}
+        renderItem={({ item }) => (
+          <View key={item.addedAt} style={styles.listItem}>
+            <Text style={{ flex: 1 }}>{item.code}</Text>
+            <Ionicons
+              name='md-color-palette'
+              size={26}
+              style={{ marginRight: 30 }}
+              color={item.color || '#555'}
+              onPress={selectItem(item)}
+            />
+            <Ionicons
+              name='md-close'
+              size={22}
+              onPress={removeItem(item)}
+              style={{ marginRight: 10 }}
+              color='#FCC'
+            />
+          </View>
+        )}
+        keyExtractor={item => item.addedAt}
+      />
+      <Modal
+        animationType='slide'
+        transparent={false}
+        visible={modalVisible}
+        presentationStyle='pageSheet'
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.')
+        }}>
+        <View style={{ marginTop: 22, flex: 1, padding: 20 }}>
+          <View style={{ flex: 1 }}>
+            <TriangleColorPicker
+              onColorSelected={color => {
+                toggleModal()
+                changeColor(modalSelected, color)
+              }}
+              defaultColor={modalSelected.color || '#555'}
+              style={{ flex: 1 }}
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  )
 }
 
-LinksScreen.navigationOptions = {
-  title: 'Links',
-};
+OptionsScreen.navigationOptions = {
+  title: 'Linhas favoritas'
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
-});
+  listItem: {
+    height: 60,
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomColor: '#EEE',
+    borderBottomWidth: 1,
+    alignItems: 'center'
+  }
+})
